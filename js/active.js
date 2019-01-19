03e08138dd2d6be423fbfc55c018d413f9516f27
Takayuki Tominaga <touka_tt@hotmail.co.jp>
2019-01-19 15:54:04 +0900

    change scroll speed

--- js/active.js
+++ js/active.js
@@ -42,11 +42,12 @@
 	$.scrollUp({
 		scrollText: '<i class="fa fa-angle-up"></i>',
 		easingType: 'linear',
-		scrollSpeed: 900,
+		scrollSpeed: 300,
 	});
 	
+	/* Smooth Scroll */
 	$('a[href^="#"]').click(function(){
-    var speed = 900;
+    var speed = 300;
     var href= $(this).attr("href");
     var target = $(href == "#" || href == "" ? 'html' : href);
     var position = target.offset().top;
