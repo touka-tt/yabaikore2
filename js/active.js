1d14a42ce4308ff315b11cb3eabdd7ddb103d903
Takayuki Tominaga <touka_tt@hotmail.co.jp>
2019-01-19 15:40:11 +0900

    smooth scroll

--- js/active.js
+++ js/active.js
@@ -44,6 +44,15 @@
 		easingType: 'linear',
 		scrollSpeed: 900,
 	});
+	
+	$('a[href^="#"]').click(function(){
+    var speed = 900;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+	});
 
 
 
