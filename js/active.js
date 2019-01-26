cd146187cd5fbae836bb7090cbd37fc22144954e
Takayuki Tominaga <touka_tt@hotmail.co.jp>
2019-01-26 17:23:56 +0900

    add kei toriki

--- js/active.js
+++ js/active.js
@@ -45,18 +45,6 @@
 		scrollSpeed: 300,
 	});
 	
-	/* Smooth Scroll */
-	$('a[href^="#"]').click(function(){
-    var speed = 300;
-    var href= $(this).attr("href");
-    var target = $(href == "#" || href == "" ? 'html' : href);
-    var position = target.offset().top;
-    $("html, body").animate({scrollTop:position}, speed, "swing");
-    return false;
-	});
-
-
-
 
 	/* Fake Loader */
 	$('.fakeloader').fakeLoader({
